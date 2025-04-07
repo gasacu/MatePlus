8fad62922704ec271c3edd92a284a79612b624a6
GabrielSacuianu <gsacuianu@gmail.com>
2025-04-07 20:12:11 +0300

    hero section stylization

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,14 +9,37 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  progress: number = 80;
+  progress: number = 90;
   completedTests: number = 5;
-  averageScore: number = 85;
+  showTestAdded = false;
+  averageScore: number = 95;
+  animatedProgress = 0;
+  circumference = 2 * Math.PI * 50;
   recentTests = [
     { name: 'Test Algebra', score: 90 },
     { name: 'Test Geometrie', score: 85 },
     { name: 'Test Trigonometrie', score: 78 },
   ];
+
+  ngOnInit() {
+    this.animateProgress();
+  }
+
+  animateProgress() {
+    const interval = setInterval(() => {
+      if (this.animatedProgress < this.progress) {
+        this.animatedProgress++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 15); // viteza
+  }
+
+  getProgressLevel(): 'bronze' | 'silver' | 'gold' {
+    if (this.progress < 50) return 'bronze';
+    if (this.progress < 75) return 'silver';
+    return 'gold';
+  }
 
   resetProgress() {
     this.progress = 0;
@@ -25,8 +48,27 @@ export class DashboardComponent {
     this.recentTests = [];
   }
 
+  addTest() {
+    this.completedTests += 1;
+    this.showTestAdded = true;
+  
+    setTimeout(() => {
+      this.showTestAdded = false;
+    }, 2000); // dispare după 2 secunde
+  }
+
   reviewTest(test: any) {
     console.log('Revizuire test:', test);
     // Aici poți adăuga logica pentru navigarea către pagina de revizuire
+  }
+
+  getScoreColor(score: number): string {
+    if (score < 50) return '#ff4d4f'; // roșu
+    if (score < 80) return '#faad14'; // portocaliu
+    return '#52c41a'; // verde
+  }
+  
+  get strokeColor(): string {
+    return this.getScoreColor(this.averageScore);
   }
 }
